2535e8e5731737ad840b5e9a1ad51c681ae64f81
Collin Town <collintown@gmail.com>
2022-11-19 10:38:09 -0500

    Added custom colors for project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'cornsilk': {
+        400: '#FFF9DF',
+      },
+      'patrick-blue': {
+        400: '#262561'
+      },
+      'cerise': {
+        400: '#EC2964'
+      },
+      'orange': {
+        400: '#FB8621'
+      }
+    },
     extend: {},
   },
   plugins: [],
