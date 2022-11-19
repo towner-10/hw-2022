bd9089a25a2821f7d3fe395a9a13a9dfc7f7a506
Collin Town <collintown@gmail.com>
2022-11-19 10:50:15 -0500

    Added custom font

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container bg-cornsilk-400">
-        <h1>Better WikiHow</h1>
+      <div className="bg-cornsilk-400 w-screen h-screen flex flex-col items-center justify-center">
+        <h1 className='text-3xl'>Better WikiHow</h1>
       </div>
     </>
   )
